28f2329c53a9c5d09a93d395a1b777569b8f6355
Neeraj <nkr2101@gmail.com>
2023-03-11 11:24:34 +0530

    second master commit

--- src/com/practice/Master.java
+++ src/com/practice/Master.java
@@ -3,7 +3,11 @@ package com.practice;
 public class Master {
 
     public void master1(){
+        System.out.println("master1");
+    }
 
+    public void master2(){
+        System.out.println("master2");
     }
 
 }
